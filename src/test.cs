c20ba8092f849340c53f514b96a3e4aeae5d7621
Bui Dang Khanh <khanh.buidang@homecredit.vn>
2020-03-26 10:45:04 +0700

    UPDATE

--- src/test.cs
+++ src/test.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
 
 namespace ConsoleCore
@@ -11,18 +12,43 @@ namespace ConsoleCore
 
         static void Main(string[] args)
         {
-            int x = 1;
-            string a = "3463";
-            var t = (x, a);
-            (_, a) = new test { X = 10, Y = "abc" };
-            var (m, n) = (1, "abc");
-            Console.WriteLine((x, a));
+            // quick declaration, tuple style.
+            var (iii, sss) = (123, "abc");
+            // write as tuple, and write normal
+            Console.WriteLine($"{(iii, sss)} | {iii} - {sss}");
+
+            Console.WriteLine("-----");
+
+            Console.WriteLine(tripleValue((iii, sss)));
+            Console.WriteLine($"{(iii, sss)}");
+
+            Console.WriteLine("-----");
+
+            var tup = (iii * 2, sss + sss);
+            Console.WriteLine(tripleValue(tup));
+            Console.WriteLine(tup);
+
+            Console.WriteLine("---- new value when assign structure ---");
+
+            var tup22 = tup;
+            tup22.Item1 *= 2;
+            Console.WriteLine(tup);
+            Console.WriteLine(tup22);
+
+            (_, sss) = new test { X = 10, Y = "abc" };
+
+            var str = "abcde";
+            Debug.Assert('e' == str[^1]);      // str[str.Length - 1]
+
             Console.ReadKey();
         }
 
-        static (int, string) tuplefunc()
+        static (int, string) tripleValue((int iVal, string sVal) tuple)
         {
-            return (33, "etwetwet");
+            tuple.iVal++;
+            tuple.sVal += tuple.sVal;
+
+            return (tuple.iVal * 3, tuple.sVal + tuple.sVal + tuple.sVal);
         }
 
         public void Deconstruct(out int firstName, out string lastName) => (firstName, lastName) = (X, Y);
